882c2c9f677e1fa1f3895fad0f54b991f890c927
Arjun Dhami <arjundhami185@gmail.com>
2024-03-09 13:24:33 +0545

    updated permissions map

--- permission/permission_maps/user_permission_map.py
+++ permission/permission_maps/user_permission_map.py
@@ -48,67 +48,49 @@ def get_anonymous_permission_map():
 
 def get_unverified_permission_map():
     return get_general_permission_map() + get_anonymous_permission_map() + [
-        USER_API,
-        USER_DELETE_API,
-        REVIEWS_READ_ONLY_API,
-        VEHICLE_REPAIR_REQUEST_READ_API,
-        CUSTOMER_CREATE_API,
+    USER_API,
+    USER_DELETE_API,
+    REVIEWS_READ_ONLY_API,
+    USER_PUBLIC_API,
+    VEHICLE_REPAIR_REQUEST_READ_API,
+    CUSTOMER_CREATE_API,
     ]
 
 
+def get_verified_permission_map():
+    return get_general_permission_map() + get_anonymous_permission_map() + get_unverified_permission_map() + [
+    REPAIR_STEP_READ_API,
+    ]
+
 def get_superuser_permission_map():
-    return get_general_permission_map() + [
-        AUTH_API,
-        VERIFY_TOKEN_API,
-        REVIEWS_READ_ONLY_API,
-        REVIEWS_WRITE_API,
-        VEHICLE_REPAIR_REQUEST_READ_API,
-        VEHICLE_REPAIR_REQUEST_WRITE_API,
-        VEHICLE_REPAIR_REQUEST_IMAGES_READ_API,
-        VEHICLE_REPAIR_REQUEST_IMAGES_WRITE_API,
-        VEHICLE_REPAIR_REQUEST_VIDEOS_READ_API,
-        VEHICLE_REPAIR_REQUEST_VIDEOS_WRITE_API,
-        USER_API,
-        USER_PUBLIC_API,
-        SERVICE_WRITE_API,
-        MECHANIC_TIPS_READ_API,
-        MECHANIC_TIPS_WRITE_API,
-        VEHICLE_CATEOGRY_READ_API,
-        VEHICLE_CATEOGRY_WRITE_API,
+    return get_general_permission_map() + get_anonymous_permission_map() + get_unverified_permission_map() + get_verified_permission_map() + get_consumer_permission_map() + get_mechanic_permission_map() +[
+    VERIFY_TOKEN_API,
+    SERVICE_WRITE_API,
+    MECHANIC_TIPS_WRITE_API,
+    VEHICLE_CATEOGRY_WRITE_API,
     ]
 
 
 def get_consumer_permission_map():
-    return get_general_permission_map() + [
-        AUTH_API,
-        REVIEWS_READ_ONLY_API,
-        REVIEWS_WRITE_API,
-        VEHICLE_REPAIR_REQUEST_READ_API,
-        VEHICLE_REPAIR_REQUEST_WRITE_API,
-        VEHICLE_REPAIR_REQUEST_IMAGES_READ_API,
-        VEHICLE_REPAIR_REQUEST_IMAGES_WRITE_API,
-        VEHICLE_REPAIR_REQUEST_VIDEOS_READ_API,
-        VEHICLE_REPAIR_REQUEST_VIDEOS_WRITE_API,
-        USER_API,
-        USER_PUBLIC_API,
-        USER_DELETE_API,
-        MECHANIC_TIPS_READ_API,
-        REPAIR_STEP_READ_API,
-        REPAIR_STEP_WRITE_API,
-        VEHICLE_CATEOGRY_READ_API,
-        USER_UPDATE_LOCATION_API,
+    return get_general_permission_map() + get_anonymous_permission_map() +[
+    REVIEWS_WRITE_API,
+    VEHICLE_REPAIR_REQUEST_READ_API,
+    VEHICLE_REPAIR_REQUEST_WRITE_API,
+    VEHICLE_REPAIR_REQUEST_IMAGES_READ_API,
+    VEHICLE_REPAIR_REQUEST_IMAGES_WRITE_API,
+    VEHICLE_REPAIR_REQUEST_VIDEOS_READ_API,
+    VEHICLE_REPAIR_REQUEST_VIDEOS_WRITE_API,
+    REPAIR_STEP_WRITE_API,
+    VEHICLE_CATEOGRY_READ_API,
+    USER_UPDATE_LOCATION_API,
     ]
 
 
 def get_mechanic_permission_map():
-    return get_general_permission_map() + [
-        AUTH_API,
-        REVIEWS_READ_ONLY_API,
-        VEHICLE_REPAIR_REQUEST_READ_API,
-        VEHICLE_REPAIR_REQUEST_IMAGES_READ_API,
-        VEHICLE_REPAIR_REQUEST_IMAGES_WRITE_API,
-        VEHICLE_REPAIR_REQUEST_VIDEOS_READ_API,
-        VEHICLE_REPAIR_REQUEST_VIDEOS_WRITE_API,
-        USER_API,
-        VEHICLE_CATEOGRY_READ_API,
+    return get_general_permission_map() + get_anonymous_permission_map() + [
+    VEHICLE_REPAIR_REQUEST_IMAGES_READ_API,
+    VEHICLE_REPAIR_REQUEST_IMAGES_WRITE_API,
+    VEHICLE_REPAIR_REQUEST_VIDEOS_READ_API,
+    VEHICLE_REPAIR_REQUEST_VIDEOS_WRITE_API,
+    VEHICLE_CATEOGRY_READ_API,
     ]
