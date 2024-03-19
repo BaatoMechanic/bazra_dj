1bc67719240ac871e04956733f447002b754c40f
Arjun Dhami <arjundhami185@gmail.com>
2024-03-19 10:28:59 +0000

    Added mobile number compulsory

--- vehicle_repair/apis/v1/vehicle_repair_request.py
+++ vehicle_repair/apis/v1/vehicle_repair_request.py
@@ -8,35 +8,51 @@ from rest_framework.response import Response
 
 from utils.mixins.base_api_mixin import BaseAPIMixin
 from vehicle_repair.models import VehicleRepairRequest
-from vehicle_repair.models.vehicle_repair_request import VehicleRepairRequestImage, VehicleRepairRequestVideo
+from vehicle_repair.models.vehicle_repair_request import (
+    VehicleRepairRequestImage,
+    VehicleRepairRequestVideo,
+)
 from vehicle_repair.serializers.service import ServiceSerializer
-from vehicle_repair.serializers.vehicle_repair_request import VehicleRepairRequestImageSerializer, VehicleRepairRequestSerializer, VehicleRepairRequestVideoSerializer
+from vehicle_repair.serializers.vehicle_repair_request import (
+    VehicleRepairRequestImageSerializer,
+    VehicleRepairRequestSerializer,
+    VehicleRepairRequestVideoSerializer,
+)
+
 # Create your views here.
 
 
 class VehicleRepairRequestViewSet(BaseAPIMixin, ModelViewSet):
 
-    queryset = VehicleRepairRequest.objects.select_related('user').all().order_by('-created_at')
+    queryset = (
+        VehicleRepairRequest.objects.select_related("user")
+        .all()
+        .order_by("-created_at")
+    )
     serializer_class = VehicleRepairRequestSerializer
 
     def get_serializer_context(self) -> Dict[str, Any]:
         return {"request": self.request}
 
-    @action(detail=True, methods=['GET'])
+    @action(detail=True, methods=["GET"])
     def service_type(self, request, idx):
         repair_request = get_object_or_404(VehicleRepairRequest, idx=idx)
         serializer = ServiceSerializer(repair_request.service_type)
         return Response(serializer.data)
 
-    @action(detail=False, methods=['GET'])
+    @action(detail=False, methods=["GET"])
     def user_recent_repair_requests(self, request):
-        repair_requests = VehicleRepairRequest.objects.filter(user=request.user, status="complete")
+        repair_requests = VehicleRepairRequest.objects.filter(
+            user=request.user, status="complete"
+        )
         serializer = VehicleRepairRequestSerializer(repair_requests, many=True)
         return Response(serializer.data)
 
-    @action(detail=False, methods=['GET'])
+    @action(detail=False, methods=["GET"])
     def user_active_repair_requests(self, request):
-        repair_requests = VehicleRepairRequest.objects.filter(user=request.user).exclude(status="complete")
+        repair_requests = VehicleRepairRequest.objects.filter(
+            user=request.user
+        ).exclude(status="complete")
         serializer = VehicleRepairRequestSerializer(repair_requests, many=True)
         return Response(serializer.data)
 
@@ -49,7 +65,9 @@ class VehicleRepairRequestImageViewSet(BaseAPIMixin, ModelViewSet):
         repair_request: VehicleRepairRequest = VehicleRepairRequest.objects.get(
             idx=self.kwargs["repair_request_idx"],
         )
-        return VehicleRepairRequestImage.objects.filter(repair_request_id=repair_request.id)
+        return VehicleRepairRequestImage.objects.filter(
+            repair_request_id=repair_request.id
+        )
 
     def get_serializer_context(self):
         return {"repair_request": self.kwargs["repair_request_idx"]}
@@ -64,4 +82,6 @@ class VehicleRepairRequestVideoViewSet(BaseAPIMixin, ModelViewSet):
         repair_request: VehicleRepairRequest = VehicleRepairRequest.objects.get(
             idx=self.kwargs["repair_request_idx"],
         )
-        return VehicleRepairRequestVideo.objects.filter(repair_request_id=repair_request.id)
+        return VehicleRepairRequestVideo.objects.filter(
+            repair_request_id=repair_request.id
+        )
