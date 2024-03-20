754ee6f3aaa6f7e5687f944e9307c2f2ba4ade6a
Arjun Dhami <arjundhami185@gmail.com>
2024-03-20 11:43:25 +0000

    change url

--- autho/urls.py
+++ autho/urls.py
@@ -1,4 +1,3 @@
-
 from django.urls import path
 from .apis import v1
 
@@ -8,16 +7,19 @@ router = routers.DefaultRouter()
 
 
 router.register("user_info", v1.UserInfoViewSet, basename="user_info")
-router.register('users_management', v1.UserManagementViewSet, basename='users_management')
-router.register("account_recovery", v1.AccountRecoveryViewSet, basename="account_recovery")
+router.register(
+    "users_management", v1.UserManagementViewSet, basename="users_management"
+)
+router.register(
+    "account_recovery", v1.AccountRecoveryViewSet.as_view(), basename="account_recovery"
+)
 
 
 urlpatterns = [
-    path('create-token/', v1.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('refresh-token/', v1.TokenRefreshView.as_view(), name='token_refresh'),
-    path('verify-token/', v1.TokenVerifyView.as_view(), name='token_verify'),
+    path("create-token/", v1.TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("refresh-token/", v1.TokenRefreshView.as_view(), name="token_refresh"),
+    path("verify-token/", v1.TokenVerifyView.as_view(), name="token_verify"),
     # path('register/', views.register_user, name='register'),
-
 ]
 
 urlpatterns += router.urls
