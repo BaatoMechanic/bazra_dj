7f860ae8afa586b5ae383d4c51a0f245b42a2196
Krishna Rimal <baatomechanic@gmail.com>
2024-03-21 01:39:42 +0000

    added celery tasks

--- autho/apis/v1/recovery.py
+++ autho/apis/v1/recovery.py
@@ -1,13 +1,12 @@
 from rest_framework.decorators import action
-from rest_framework.generics import GenericAPIView
-
+from rest_framework.viewsets import GenericViewSet
 from autho.models.recovery_code import RecoveryCode
 from autho.serializers.recovery import SendRecoveryCodeSerializer
 from utils.api_response import api_response_success
-from utils.mixins.base_model_mixin import BaseModelMixin
+from utils.mixins.base_api_mixin import BaseAPIMixin
 
 
-class AccountRecoveryViewSet(BaseModelMixin, GenericAPIView):
+class AccountRecoveryViewSet(BaseAPIMixin, GenericViewSet):
 
     @action(detail=False, methods=["POST"])
     def send_otp(self, request, *args, **kwargs):
@@ -15,15 +14,13 @@ class AccountRecoveryViewSet(BaseModelMixin, GenericAPIView):
         serializer.is_valid(raise_exception=True)
 
         user = serializer.validated_data.get("user_identifier")
-
         code: RecoveryCode = user.gen_recovery_code()
-
         code.send()
 
         return api_response_success({"detail": "OTP sent successfully."})
 
     @action(detail=False, methods=["POST"])
-    def resend(self, request):
+    def resend(self, request, *args, **kwargs):
         recovery_idx = request.data.get("idx")
         try:
             code = RecoveryCode.objects.get(idx=recovery_idx)
