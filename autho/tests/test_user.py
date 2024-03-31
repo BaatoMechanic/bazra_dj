e479add61a6b97e32ed26bd3b19bf12466343411
Krishna Rimal <baatomechanic@gmail.com>
2024-03-31 08:22:10 +0000

    fix setup

--- autho/tests/test_user.py
+++ autho/tests/test_user.py
@@ -3,27 +3,26 @@ from django.test import TestCase
 from autho.admin import User
 
 
-# Create your tests here.
 class TestGetUserByIdentifier(TestCase):
 
-    def setup(self):
-
+    def setUp(self):
+        super().setUp()
         self.user = User.objects.create_user(phone="1234567890", password="test1234")
         self.user1 = User.objects.create_user(email="abc@abc.com", password="test1234")
 
     def test_for_invaild_identifier(self):
-        id = "1234567890"
+        id = "1287898765"
         result = User.get_user_by_identifier(id)
         self.assertEquals(result, None)
 
     def test_for_vaild_phone_number(self):
         id = "1234567890"
-        result = User.get_user_by_identifier(phone=id)
+        result = User.get_user_by_identifier(id)
         self.assertEquals(result, self.user)
 
     def test_for_vaild_email(self):
         id = "abc@abc.com"
-        result = User.get_user_by_identifier(email=id)
+        result = User.get_user_by_identifier(id)
         self.assertEquals(result, self.user1)
 
 
